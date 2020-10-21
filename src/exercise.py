0260558c8868a2967aa4cc170795e5f08521d487
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 21:39:39 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    intList = []
+    cardinality=0
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+    sum = 0
+    for i in range(len(intList)):
+      sum+=intList[i]
+      cardinality+=1
+    print("Average: %s"%(sum/cardinality))
 
 if __name__ == '__main__':
     main()
